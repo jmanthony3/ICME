0ef74d6c938afe87fd46c31919cc282e30c31eda
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-05-20 12:26:02 -0400

    cleanup output graphs.

--- 2-AtomisticsToDislocationMobility/Calculations/1-DislocationVelocity/dislocation_velocity.py
+++ 2-AtomisticsToDislocationMobility/Calculations/1-DislocationVelocity/dislocation_velocity.py
@@ -7,12 +7,12 @@ import pandas as pd
 import sympy as sp
 import sys
 import warnings
-# warnings.filterwarnings("ignore", module="matplotlib\..*")
 warnings.filterwarnings(action="ignore", module="numpy/*", message="Empty input file", category=UserWarning)
 
 filepath = os.path.dirname(os.path.abspath(__file__))
 
-# 194.8
+print("")
+
 dt, width = 0.500, 40 * 5   # lattice distance * unit distance (approx)
 offset      = 40.0
 L_buff      = 0.0 + offset
@@ -21,13 +21,11 @@ midpoint    = width / 2
 
 lattice_parameter = 2.781e-10 # m
 slip_direction = np.array([1, 1, 1])
-burger_vec = lattice_parameter*(nm.norms(slip_direction).l_two()/2) # (np.sqrt(3)/2)
-print(r"Magnitude of Burger's vector, $\|\vec{b}\|$ = " + f"{burger_vec}")
+burger_vec = lattice_parameter*(nm.norms(slip_direction).l_two()/2)
+print("Magnitude of Burger's vector, ||b|| = " + f"{burger_vec} m", end="\n\n")
 
 # must be same examined in `rescale_commands.sh`
-# if not a range, then define as blank string: TEMP = ""
 TEMP = np.array([300]) # np.arange(150, 550, 50) # K
-# if not a range, then define as blank string: SIGMA = ""
 SIGMA = np.arange(25, 325, 25) # MPa
 # for s in np.array([700, 800, 900, 1000, 1100, 1200]):
 #     SIGMA = np.append(SIGMA, s)
@@ -35,7 +33,6 @@ SIGMA = np.arange(25, 325, 25) # MPa
 skip = 1
 columns = ["Particle Identifier", "X", "Y", "Z", "Centrosymmetry", "R", "G", "B", "nan", "Time"]
 
-do_every = done_every = False
 DRAG = {}
 
 old_stdout = sys.stdout
@@ -57,46 +54,19 @@ try:
             )
             f.write(f"Stress Velocity\n")
             for sigma in SIGMA:
+                print(f"Looking at {temp:3} K, {sigma:5.3f} MPa...", end="\r")
                 x_ave, lap_count = 0, 0
                 data_dir = f"{filepath}/PositionFrameData/{temp}/{sigma:.3f}"
-                if do_every:
-                    particles = {}
-                    for i in range(0, len(next(os.walk(data_dir))[2]), skip):
-                        with open(f"{data_dir}/lammps.{i}.xyz") as g:
-                            try:
-                                py_array = np.genfromtxt(g.readlines()[2:])
-                                data = pd.DataFrame(py_array, columns=columns)
-                                particles[i] = np.array(data["Particle Identifier"])
-                            except ValueError:
-                                particles[i] = np.array([])
-
-                    particle_ids = {}
-                    for frame in particles:
-                        ids = particles[frame]
-                        for id in ids:
-                            particle_ids[int(id)] = []
-                    for frame in particles:
-                        ids = particles[frame]
-                        for id in ids:
-                            particle_ids[int(id)].append(frame)
-                    foo = 0
-                    for ids in particle_ids:
-                        if len(particle_ids[ids]) == 1:
-                            foo += 1
-                        else: foo -= 1
-                    if foo == len(particle_ids): done_every = False
-                    else: done_every = True
 
                 velocities, offsets = [], []
-                if done_every:
-                    for id in particle_ids:
-                        i, time, position = 0, [], []
-                        for frame in particle_ids[int(id)]:
-                            with open(f"{data_dir}/lammps.{frame}.xyz") as g:
-                                py_array = np.genfromtxt(g.readlines()[2:])
+                i, time, position = 0, [], []
+                for frame in range(0, len(next(os.walk(data_dir))[2]), skip):
+                    with open(f"{data_dir}/lammps.{frame}.xyz") as g:
+                        try:
+                            py_array = np.genfromtxt(g.readlines()[2:])
+                            if len(py_array) > 0:
                                 data = pd.DataFrame(py_array, columns=columns)
-                                time.append(frame*dt)#np.average(data["Time"]))
-                                # position.append(np.average(data[["X"]]))
+                                time.append(frame*dt)
                                 x_store = data[["X"]].values
                                 # check to see if the dsl cluster is on wraping both sides
                                 if min(x_store) < L_buff and R_buff < max(x_store):
@@ -111,98 +81,47 @@ try:
                                     lap_count += 1
                                     x_ave += width
                                 position.append(x_ave)
-                        time, position = np.array(time), np.array(position)/10 # ps, nm
-                        # i, position2 = 0, position - position[0]
-                        # for i in range(1, len(position)):
-                        #     position2[i] = position2[i-1] + np.abs(position[i] - position[i-1])
-                        # position = position2
-                        ax_sigma.scatter(time, position)
+                        except ValueError: pass
+                        except UserWarning: pass
+                    i += 1
+                time, position = np.array(time), np.array(position)/10 # ps, nm
+                ax_sigma.scatter(time, position)#, label="Raw")
 
-                        try:
-                            position_expr = nm.least_squares.linear(time, position, 1)[0]
-                            other_pos = np.array(position_expr(t) for t in time)
-                            # ax_sigma.plot(time, other_pos)
+                try:
+                    position_expr = nm.least_squares.linear(time, position, 1)[0]
+                    other_pos = np.array([position_expr(t) for t in time])
+                    # ax_sigma.plot(time, other_pos, "k", label="Homogeneous Fit")
 
-                            if "t" != str(position_expr(sp.Symbol("t")))[-1]:
-                                velocity = float(str(position_expr(sp.Symbol("t"))).split(" ")[0].split("*")[0])
-                                offset = float(str(position_expr(sp.Symbol("t"))).split(" ")[-1])
-                            else :
-                                velocity = float(str(position_expr(sp.Symbol("t"))).split(" ")[-1].split("*")[0])
-                                offset = float(str(position_expr(sp.Symbol("t"))).split(" ")[0])
-                            velocities.append(velocity)
-                            offsets.append(offset)
-                        except np.linalg.LinAlgError: pass
-                else:
-                    i, time, position = 0, [], []
-                    for frame in range(0, len(next(os.walk(data_dir))[2]), skip):
-                        with open(f"{data_dir}/lammps.{frame}.xyz") as g:
-                            try:
-                                py_array = np.genfromtxt(g.readlines()[2:])
-                                if len(py_array) > 0:
-                                    data = pd.DataFrame(py_array, columns=columns)
-                                    time.append(frame*dt)#np.average(data["Time"]))
-                                    # position.append(np.average(data[["X"]]))
-                                    x_store = data[["X"]].values
-                                    # check to see if the dsl cluster is on wraping both sides
-                                    if min(x_store) < L_buff and R_buff < max(x_store):
-                                        for j in range(len(x_store)):
-                                            #values on left side (wrapped around)
-                                            if x_store[j] < midpoint:
-                                                x_store[j] = x_store[j] + width
-                                    x_ave_prev = x_ave
-                                    x_ave = np.average(x_store) + width*lap_count
-                                    # check if a lap just happened
-                                    if x_ave < (x_ave_prev - midpoint):
-                                        lap_count += 1
-                                        x_ave += width
-                                    position.append(x_ave)
-                            except ValueError: pass
-                            except UserWarning: pass
-                            i += 1
-                    time, position = np.array(time), np.array(position)/10 # ps, nm
-                    # i, position2 = 0, position - position[0]
-                    # for i in range(1, len(position2)):
-                    #     position2[i] = position2[i-1] + np.abs(position[i] - position[i-1])
-                    # position = position2
-                    ax_sigma.scatter(time, position)#, label="Raw")
-
-                    try:
-                        position_expr = nm.least_squares.linear(time, position, 1)[0]
-                        other_pos = np.array([position_expr(t) for t in time])
-                        # ax_sigma.plot(time, other_pos, "k", label="Homogeneous Fit")
-
-                        if "t" != str(position_expr(sp.Symbol("t")))[-1]:
-                            velocity = float(str(position_expr(sp.Symbol("t"))).split(" ")[0].split("*")[0])
-                            offset = float(str(position_expr(sp.Symbol("t"))).split(" ")[-1])
-                        else:
-                            velocity = float(str(position_expr(sp.Symbol("t"))).split(" ")[-1].split("*")[0])
-                            offset = float(str(position_expr(sp.Symbol("t"))).split(" ")[0])
-                        velocities.append(velocity)
-                        offsets.append(offset)
-                    except np.linalg.LinAlgError: pass
+                    if "t" != str(position_expr(sp.Symbol("t")))[-1]:
+                        velocity = float(str(position_expr(sp.Symbol("t"))).split(" ")[0].split("*")[0])
+                        offset = float(str(position_expr(sp.Symbol("t"))).split(" ")[-1])
+                    else:
+                        velocity = float(str(position_expr(sp.Symbol("t"))).split(" ")[-1].split("*")[0])
+                        offset = float(str(position_expr(sp.Symbol("t"))).split(" ")[0])
+                    velocities.append(velocity)
+                    offsets.append(offset)
+                except np.linalg.LinAlgError: pass
 
                 velocity = np.average(velocities)
                 f.write(f"{sigma:.9f} {velocity*1e3:.9f}\n") # MPa m/s
                 offset = np.average(offsets)
 
                 try:
-                    pft_string = f"{engr(velocity, 3)}*t + {engr(offset, 3)}"
-                    # ax_sigma.text(
-                    #     x=ax_sigma.get_xlim()[1]*0.65,
-                    #     y=ax_sigma.get_ylim()[1]*0.05,
-                    #     s=pft_string
-                    # )
+                    pft_string = f"{round(velocity, 3)}*t + {round(offset, 3)}"
                 except ValueError:
                     pft_string = f"Could not round..."
-                    # ax_sigma.text(
-                    #     x=ax_sigma.get_xlim()[1]*0.65,
-                    #     y=ax_sigma.get_ylim()[1]*0.05,
-                    #     s=pft_string
-                    # )
                 time = np.arange(0, 100, dt*skip)
                 other_pos = velocity*time + offset
-                ax_sigma.plot(time, other_pos, label=f"{sigma} MPa: {pft_string}")
-            ax_sigma.legend()
+                ax_sigma.plot(time, other_pos, label=f"{int(sigma)}".zfill(4) + f" MPa | {pft_string}".ljust(15))
+                print(f"Closing {temp} K, {sigma:.3f} MPa...", end="\r")
+            ax_sigma.text(
+                x=ax_sigma.get_xlim()[1]*0.175,
+                y=ax_sigma.get_ylim()[1]*-0.025,
+                s=r"The $v$'s in legend equations are dislocation velocities."
+            )
+            ax_sigma.legend(
+                title=r"$x(t) = v[\frac{nm}{ps}]*t[ps] + [nm]$"
+            )
             fig_sigma.savefig(f"position_for_time-{temp}.svg")
         with open(f"dislocation_velocity-{temp}", "r") as f:
             data = pd.DataFrame(np.genfromtxt(f.readlines())[1:], columns=["Stress", "Velocity"])
@@ -215,12 +134,11 @@ finally:
     sys.stdout.close()
     sys.stdout = old_stdout
 
-    # print(DRAG)
-
     # Print the names of the columns.
-    print ("{:<10} {:<10}".format(r"Temperature [$K$]", r"Drag Coefficient ($B$) [$Pa-s$]", r"Dislocation Mobility ($M_{s}$) [$\frac{1}{Pa-s}$]"))
+    print("{:<15} | {:<30} | {:<35}".format("Temperature [K]", "Drag Coefficient (B) [Pa-s]", "Dislocation Mobility (Ms) [1/Pa-s]"))
+    print("-"*16 + "|" + "-"*32 + "|" + "-"*36)
 
     # print each data item.
     for key, value in DRAG.items():
         coeff, mob = value
-        print ("{:<10} {:<10} {:<10}".format(key, coeff, mob))
+        print("{:<15} | {:<30} | {:<35}".format(key, coeff, mob))
