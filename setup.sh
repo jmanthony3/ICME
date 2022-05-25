b9bbab24c60278886dca39594bb5f19d127ba165
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-05-25 16:56:22 -0400

    echo which bridge

--- setup.sh
+++ setup.sh
@@ -20,9 +20,13 @@ who=$(whoami)
 
 ########################### BRIDGES ###########################
 ### setup bridges
+echo "Setting up '1-ElectronicsToAtomistics' bridge..."
 cd "1-ElectronicsToAtomistics"; ./setup.sh # 1
+clear; echo "Setting up '2-AtomisticsToDislocationMobility' bridge..."
 cd "../2-AtomisticsToDislocationMobility"; ./setup.sh # 2
+clear; echo "Setting up '3-DislocationMobilityToCrystalPlasticity' bridge..."
 cd "../3-DislocationMobilityToCrystalPlasticity"; ./setup.sh # 3
+echo "Done."
 
 
 
