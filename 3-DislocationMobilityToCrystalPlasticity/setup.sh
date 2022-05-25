b8ae2226e8eb9ec4127f4a49d4f9a3d008de52c2
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-05-25 13:09:32 -0400

    create setup script

---
+++ 3-DislocationMobilityToCrystalPlasticity/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# - - - - - - - END OF HUMAN EDITABLE SECTION - - - - - - - - #
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+
+
+
+
+set +x
+execution_dir=$(pwd)
+who=$(whoami)
+
+
+
+# copy/paste necessary files for example
+cd "$execution_dir/Files/"
+rm -r "test/" || mkdir "test/"
+rm -r "test/RescaleUpload/" || mkdir "test/RescaleUpload/"
+cp "bcc.sx" "./test/RescaleUpload/"
+cp "numbers.inc" "./test/RescaleUpload/"
+cp "params_xtal.inc" "./test/RescaleUpload/"
+cp "rve.single.inp" "./test/RescaleUpload/"
+cp "texture.txti" "./test/RescaleUpload/"
+cp "umat_xtal.f" "./test/RescaleUpload/"
+cp "test.xtali" "./test/RescaleUpload/"
+
+### populate "../Calculations/0-Scripts/" folder
+cp "rescale_commands.sh" "../Calculations/3-DislocationMobilityToCrystalPlasticity/0-Scripts/"
+
+
+
+
+
+# that's all folks
