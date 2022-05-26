ab1d3fb71cb2cf73692a50af34f674980f69db25
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-05-26 11:54:17 -0400

    simplify duplicate log files.

--- setup.sh
+++ setup.sh
@@ -16,9 +16,6 @@ set +x
 execution_dir=$(pwd)
 who=$(whoami)
 mkdir "$execution_dir/logs"
-mkdir "$execution_dir/logs/1-ElectronicsToAtomistics"
-mkdir "$execution_dir/logs/2-AtomisticsToDislocationMobility"
-mkdir "$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity"
 
 
 
@@ -28,25 +25,19 @@ mkdir "$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity"
 echo "Setting up '1-ElectronicsToAtomistics' bridge..."
 echo "+ $execution_dir/1-ElectronicsToAtomistics/setup.sh"
 cd "$execution_dir/1-ElectronicsToAtomistics"
-(set -x; ./setup.sh) \
-    1>"$execution_dir/logs/1-ElectronicsToAtomistics/setup.log" \
-    2>"$execution_dir/logs/1-ElectronicsToAtomistics/errors.log"
+(set -x; ./setup.sh) &>"$execution_dir/logs/1-ElectronicsToAtomistics.log"
 
 # bridge: 2
 clear; echo "Setting up '2-AtomisticsToDislocationMobility' bridge..."
 echo "+ $execution_dir/2-AtomisticsToDislocationMobility/setup.sh"
 cd "$execution_dir/2-AtomisticsToDislocationMobility"
-(set -x; ./setup.sh) \
-    1>"$execution_dir/logs/2-AtomisticsToDislocationMobility/setup.log" \
-    2>"$execution_dir/logs/2-AtomisticsToDislocationMobility/errors.log"
+(set -x; ./setup.sh) &>"$execution_dir/logs/2-AtomisticsToDislocationMobility.log"
 
 # bridge: 3
 clear; echo "Setting up '3-DislocationMobilityToCrystalPlasticity' bridge..."
 echo "+ $execution_dir/3-DislocationMobilityToCrystalPlasticity"
 cd "$execution_dir/3-DislocationMobilityToCrystalPlasticity"
-(set -x; ./setup.sh) \
-    1>"$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity/setup.log" \
-    2>"$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity/errors.log"
+(set -x; ./setup.sh) &>"$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity.log"
 
 echo "Done."
 
