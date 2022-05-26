2130862b2e8c8d7b7b590cc54e39832adde2681d
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-05-26 11:30:02 -0400

    fix execution directories

--- setup.sh
+++ setup.sh
@@ -25,21 +25,26 @@ mkdir "$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity"
 ########################### BRIDGES ###########################
 ### setup bridges
 # bridge: 1
-clear; echo "Setting up '1-ElectronicsToAtomistics' bridge..."
-(set -x; $execution_dir/1-ElectronicsToAtomistics/setup.sh) \
+echo "Setting up '1-ElectronicsToAtomistics' bridge..."
+echo "+ $execution_dir/1-ElectronicsToAtomistics/setup.sh"
+cd "$execution_dir/1-ElectronicsToAtomistics"
+(set -x; ./setup.sh) \
     1>"$execution_dir/logs/1-ElectronicsToAtomistics/setup.log" \
     2>"$execution_dir/logs/1-ElectronicsToAtomistics/errors.log"
 
 # bridge: 2
 clear; echo "Setting up '2-AtomisticsToDislocationMobility' bridge..."
-(set -x; $execution_dir/2-AtomisticsToDislocationMobility/setup.sh) \
+echo "+ $execution_dir/2-AtomisticsToDislocationMobility/setup.sh"
+cd "$execution_dir/2-AtomisticsToDislocationMobility"
+(set -x; ./setup.sh) \
     1>"$execution_dir/logs/2-AtomisticsToDislocationMobility/setup.log" \
     2>"$execution_dir/logs/2-AtomisticsToDislocationMobility/errors.log"
 
 # bridge: 3
-echo "Setting up '3-DislocationMobilityToCrystalPlasticity' bridge..."
-cd "../3-DislocationMobilityToCrystalPlasticity"
-(set -x; $execution_dir/3-DislocationMobilityToCrystalPlasticity/setup.sh) \
+clear; echo "Setting up '3-DislocationMobilityToCrystalPlasticity' bridge..."
+echo "+ $execution_dir/3-DislocationMobilityToCrystalPlasticity"
+cd "$execution_dir/3-DislocationMobilityToCrystalPlasticity"
+(set -x; ./setup.sh) \
     1>"$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity/setup.log" \
     2>"$execution_dir/logs/3-DislocationMobilityToCrystalPlasticity/errors.log"
 
