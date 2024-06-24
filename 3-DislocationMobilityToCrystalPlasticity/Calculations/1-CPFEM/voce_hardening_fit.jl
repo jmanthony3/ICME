916ddfb20e18d19b0c3bafe56d56e05e1914d699
Joby M. Anthony III <jmanthony1@liberty.edu>
2024-06-24 14:33:01 -0400

    translate from Python to Julia

--- 3-DislocationMobilityToCrystalPlasticity/Calculations/1-CPFEM/voce_hardening_fit.jl
+++ 3-DislocationMobilityToCrystalPlasticity/Calculations/1-CPFEM/voce_hardening_fit.jl
@@ -1,81 +1,81 @@
-from engineering_notation import EngNumber as engr
-import joby_m_anthony_iii.numerical_methods as nm
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import pandas as pd
-import sympy as sp
-import sys
-import warnings
-# warnings.filterwarnings("ignore", module="matplotlib\..*")
-# warnings.filterwarnings(action="ignore", module="numpy/*", message="Empty input file", category=UserWarning)
+using CSV
+using DataFrames
+using LinearAlgebra: norm
+using NumericalMethods
+using Plots; gr()
+
+default(grid=false, framestyle=:box)
 
 lattice_parameter = 2.781e-10 # m
-slip_direction = np.array([1, 1, 1])
-burger_vec = lattice_parameter*(nm.Norm(slip_direction).l_two()/2) # (np.sqrt(3)/2)
+slip_direction = [1., 1., 1.]
+burger_vec = lattice_parameter*(norm(slip_direction) / 2.) # (np.sqrt(3)/2)
 # print(burger_vec)
 mu = 195.343e3 # 117e3 # MPa
 
 # must be same examined in `rescale_commands.sh`
 # if not a range, then define as blank string: TEMP = ""
-TEMP = np.array([300]) # np.arange(150, 500, 50) # K
+TEMP = [300.] # float.(range(150, 500; step=50)) # K
 # if not a range, then define as blank string: SIGMA = ""
-STRAIN = np.array([1000]) # np.arange(25, 325, 25) # MPa
-# for s in np.array([700, 800, 900, 1000, 1100, 1200]):
-#     SIGMA = np.append(SIGMA, s)
+STRAIN = [1000.] # float.(range(25, 325; step=25)) # MPa
+# append!(SIGMA, float.([700, 800, 900, 1000, 1100, 1200]))
 
-skip = int(1.5e2)
+skip = 150
 columns = ["timenow", "disDensity", "Stress", "Strain", "S1", "S2", "S3", "S23", "S31", "S12", "p1", "p2", "p3", "p23", "p31",  "p12", "jogs", "junctions", "CrossSlip"]
 
-do_monitor = False
+do_monitor = false
 
-H0 = np.arange(0.4, 0.42, 0.0001)*1e6 # Pa
-# H0 = np.arange(0, 101, 1)*1e6 # Pa
-C = np.array([1000]) # np.arange(73.5, 73.61, 0.01) # 1/s
-# C = np.arange(0, 101, 1) # 1/s
-Kappa = lambda ks, k0, h0, c, t: ks - (ks - k0)*np.exp(-h0/(ks - k0)*c*t)
+H0 = range(0.4, 0.42; step=0.0001) .* 1e6 # Pa
+# H0 = range(0, 101; step=1) .* 1e6 # Pa
+C = [1000.] # float.(range(73.5, 73.61; step=0.01)) # 1/s
+# C = float.(range(0, 101; step=1)) # 1/s
+Kappa(ks, k0, h0, c, t) = ks - ((ks - k0) * exp(-h0 / (ks - k0) * c * t))
 
 # old_stdout = sys.stdout
 # sys.stdout = open(os.devnull, "w")
 # try:
 fig, (ax_kappa, ax_alpha) = plt.subplots(1, 2)
 # fig, ax_kappa = plt.subplots(1, 1)
-ax_kappa.set(
+ax_kappa = plot(
+    xlabel="Normalized True Strain (\$\\epsilon\$)",
+    # xscale=:log10,
+    ylabel="True Stress (\$\\sigma\$) [\$MPa\$]",
+    # yscale=:log10,
     title="Stress-Strain @ 300 K",
-    # xscale="symlog",
-    xlabel=r"Normalized True Strain ($\epsilon$)",
-    # yscale="symlog",
-    ylabel=r"True Stress ($\sigma$) [$MPa$]",
 )
-ax_alpha.set(
-    title="Average Value of\nJunction Strength @ 300 K",
+ax_alpha = plot(
     # xlim=(0, 0.0002),
-    # xscale="symlog",
-    xlabel=r"Time ($t$) [$\mu s$]",
-    # yscale="symlog",
-    ylabel=r"Junction Strength ($\alpha$)",
+    xlabel="Time (\$t\$) [\$\\mu-s\$]",
+    # xscale=:log10,
+    ylabel="Junction Strength (\$\\alpha\$)",
+    yguide_position=:right,
+    # yscale=:log10,
+    ymirror=true,
+    title="Average Value of\nJunction Strength @ 300 K",
 )
-ax_alpha.yaxis.set_label_position("right")
-if do_monitor:
-    # MDDP_BCC_HW2/MDDP_Zip/Windows/
-    with open(f"../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/DDtimeResults.out", "r") as f:
-    # with open(f"./input", "r") as f:
-        data = pd.DataFrame(np.genfromtxt(f.readlines()[4:]), columns=columns)
+if do_monitor
+    while do_monitor
+        # # MDDP_BCC_HW2/MDDP_Zip/Windows/
+        # open(f"../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/DDtimeResults.out", "r") do f
+        # # with open(f"./input", "r") as f:
+        # end
+        data = CSV.read("../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/DDtimeResults.out", DataFrame; header=columns, skipto=4)
         # data.plot(
         #     kind="scatter",
         #     x="Strain",
         #     y="Stress",
         #     color="blue",
         #     ax_kappa=plt.gca())
-        ax_kappa.scatter(
-            np.array([data["Strain"][i] for i in range(0, len(data), skip)]),#[:int(1e1)],
-            np.array([data["Stress"][i] for i in range(0, len(data), skip)])/1e6,#[:int(1e1)]/1e6
+        scatter!(ax_kappa,
+            data[!, "Strain"][begin:skip:end], # [:int(1e1)],
+            data[!, "Stress"][begin:skip:end] ./ 1e6, # [:int(1e1)]/1e6
             ) # label=f"{strain} " + r"$\frac{1}{s}$")
-    # ax_kappa.legend()
-    fig.savefig(f"./ENGR851_JobyAnthonyIII_Homework3/stress_strain-monitor.png")
-else:
-    for temp in TEMP:
-        for strain in STRAIN:
+        # ax_kappa.legend()
+        savefig("./ENGR851_JobyAnthonyIII_Homework3/stress_strain-monitor.png")
+        sleep(30)
+    end
+else
+    for temp in TEMP
+        for strain in STRAIN
             # # MDDP_BCC_HW2/MDDP_Zip/Windows/
             # with open(f"../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/{temp}/{strain}/ResultPerSlip", "r") as f:
             #     data = f.readlines()
@@ -87,83 +87,78 @@ else:
             #         line += 1
             # f.close()
             # lines = np.array(lines)
-            with open(f"../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/{temp}/{strain}/DDtimeResults.out", "r") as f:
-            # with open(f"./input", "r") as f:
-                data = pd.DataFrame(np.genfromtxt(f.readlines()[4:]), columns=columns)
-                # print(len(lines), len(data))
-                # lines = lines[:len(data)+1]
-                # with open(f"../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/{temp}/{strain}/ResultPerSlip", "r") as g:
-                #     slip = g.readlines()
-                #     i, rho_f = 0, np.zeros(len(lines)) # int, 1/m2
-                #     for line in lines[:-2]:
-                #         rho_f[i] = np.average(np.genfromtxt(slip[line+2:lines[i+1]-1])[:,1])
-                #         i += 1
-                #     rho_f[i] = np.average(np.genfromtxt(slip[lines[-2]+2:lines[-1]-1])[:,1])
-                # g.close()
-                time = np.array([data["timenow"][i] for i in range(0, len(data), skip)])
-                eps = np.array([data["Strain"][i] for i in range(0, len(data), skip)])#/data["Strain"].values[-1],#[:int(1e1)],
-                sigma = np.array([data["Stress"][i] for i in range(0, len(data), skip)])/1e6#[:int(1e1)]/1e6
-                # rho_fp = np.array([rho_f[i] for i in range(0, len(data), skip)])
-                # rho_f = rho_fp
-                rho_f = np.array([data["disDensity"][i] for i in range(0, len(data), skip)])
-                # data.plot(
-                #     kind="scatter",
-                #     x="Strain",
-                #     y="Stress",
-                #     color="blue",
-                #     ax_kappa=plt.gca())
-                ax_kappa.scatter(
-                    eps,
-                    sigma,
-                    label=r"$\epsilon_{f}$ = " + f"{eps[-1]} @ " + r"$\dot{\epsilon}$ = " + f"{strain} " + r"$\frac{1}{s}$"
-                )
-            ERRORS = {}
-            for j in range(0, 15, 1):
-                ERRORS[f"{j}"] = {}
-                for h0 in H0:
-                    ERRORS[f"{j}"][f"{float(h0/1e6)}"] = {}
-                    for c in C:
-                        i, kappa = 0, np.zeros(len(time))
-                        error = np.zeros_like(kappa)
-                        for t in time:
-                            kappa[i] = Kappa(np.average(sigma[-j:]), sigma[0], h0, c, t)
-                            if i > 0:
-                                error[i] = np.abs((kappa[i] - sigma[i])/sigma[i])
-                            i += 1
-                        ERRORS[f"{j}"][f"{float(h0/1e6)}"][f"{c}"] = np.sum(error)
+            # open("../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/$temp/$strain/DDtimeResults.out", "r") do f
+            # # with open(f"./input", "r") as f:
+            # end
+            data = CSV.read("../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/$temp/$strain/DDtimeResults.out", DataFrame; header=columns, skipto=4)
+            # print(len(lines), len(data))
+            # lines = lines[:len(data)+1]
+            # with open(f"../Homework 2/MDDP_BCC_HW2/MDDP_Zip/Windows/{temp}/{strain}/ResultPerSlip", "r") as g:
+            #     slip = g.readlines()
+            #     i, rho_f = 0, np.zeros(len(lines)) # int, 1/m2
+            #     for line in lines[:-2]:
+            #         rho_f[i] = np.average(np.genfromtxt(slip[line+2:lines[i+1]-1])[:,1])
+            #         i += 1
+            #     rho_f[i] = np.average(np.genfromtxt(slip[lines[-2]+2:lines[-1]-1])[:,1])
+            # g.close()
+            time = data[!, "timenow"][begin:skip:end]
+            eps = data[!, "Strain"][begin:skip:end] # /data["Strain"].values[-1],#[:int(1e1)],
+            sigma = data[!, "Stress"][begin:skip:end] ./ 1e6 # [:int(1e1)]/1e6
+            # rho_fp = np.array([rho_f[i] for i in range(0, len(data), skip)])
+            # rho_f = rho_fp
+            rho_f = data[!, "disDensity"][begin:skip:end]
+            # data.plot(
+            #     kind="scatter",
+            #     x="Strain",
+            #     y="Stress",
+            #     color="blue",
+            #     ax_kappa=plt.gca())
+            scatter!(ax_kappa,
+                eps, sigma,
+                label="\$\\epsilon_{f}\$ = $(last(eps)) @ \$\\dot{\\epsilon}\$ = $strain \$\\frac{1}{s}\$")
+            ERRORS = Dict{String, Any}()
+            for j in 0:1:15
+                ERRORS["$j"] = Dict{String, Any}()
+                for h0 in H0
+                    ERRORS["$j"]["$(h0 / 1e6)"] = Dict{String, Any}()
+                    for c in C
+                        kappa, error = zeros(length(time)), zeros(length(time))
+                        for (i, t) in enumerate(time)
+                            kappa[i] = Kappa(average(sigma[end-j:end]), first(sigma), h0, c, t)
+                            if i > 1
+                                error[i] = abs((kappa[i] - sigma[i]) / sigma[i])
+                            end
+                        end
+                        ERRORS["$j"]["$(h0 / 1e6)"]["$c"] = sum(error)
+                    end
+                end
+            end
             min_error = ["", "", "", 1e6]
-            for j in range(0, 15, 1):
-                for h0 in ERRORS[f"{j}"]:
-                    for c in ERRORS[f"{j}"][h0]:
-                        if ERRORS[f"{j}"][h0][c] < min_error[-1]:
-                            min_error = [j, h0, c, ERRORS[f"{j}"][h0][c]]
+            for j in 0:1:15, h0 in ERRORS["$j"], c in ERRORS["$j"][h0]
+                if ERRORS["$j"][h0][c] < last(min_error)
+                    min_error = [j, h0, c, ERRORS["$j"][h0][c]]
+                end
+            end
             print(min_error)
             j, h0, c, error = min_error
-            i, kappa = 0, np.zeros(len(time))
-            alpha = np.zeros_like(kappa)
-            for t in time:
-                kappa[i] = Kappa(np.average(sigma[-j:]), sigma[0], float(h0)*1e6, float(c), t)
-                alpha[i] = kappa[i]/(mu*burger_vec*np.sqrt(rho_f[i]))
-                i += 1
-            print(kappa[0])
+            kappa, alpha = np.zeros(length(time)), zeros(length(time))
+            for (i, t) in enumerate(time)
+                kappa[i] = Kappa(average(sigma[end-j:end]), first(sigma), parse(Float64, h0) * 1e6, parse(Float64, c), t)
+                alpha[i] = kappa[i] / (mu * burger_vec * sqrt(rho_f[i]))
+            end
+            print(first(kappa))
             # foo = nm.MultiVariableIteration(Kappa, np.array([h0, C]), kappa)
-            ax_kappa.scatter(
-                eps,
-                kappa,
-                label=r"Voce Equation, $\kappa = \kappa_{s} - (\kappa_{s} - \kappa_{0})\exp(-\frac{h_{0}}{\kappa_{s} - \kappa_{0}}Ct)$"
-            )
-            ax_kappa.text(
-                x=ax_kappa.get_xlim()[1]*0.65,
-                y=ax_kappa.get_ylim()[1]*0.4,
-                s=r"$\kappa_{s}$ = " + f"{round(np.average(sigma[-j:]), 4)} MPa\n" + r"$\kappa_{0}$ = " + f"{round(sigma[0], 4)} MPa\n" + r"$C$ = " + f"{int(c)} " + r"$\frac{1}{s}$" + f"\n" + r"$h_{0}$ = " + f"{round(float(h0), 4)} MPa\n" + f"E = {round(error, 4)} " + r"$\frac{MPa}{MPa}$"
-            )
-            ax_alpha.scatter(
-                time*1e3,
-                alpha
-            )
-        ax_kappa.legend()
-        fig.savefig(f"./ENGR851_JobyAnthonyIII_Homework3/stress_strain-{temp}.png")
+            scatter!(ax_kappa,
+                eps, kappa,
+                label="Voce Equation, \$\\kappa = \\kappa_{s} - (\\kappa_{s} - \\kappa_{0})\\exp(-\\frac{h_{0}}{\\kappa_{s} - \\kappa_{0}}Ct)\$",
+                annotation=(0.65, 0.4,
+                    "\$\\kappa_{s}\$ = $(round(average(sigma[end-j:end]); digits=4)) MPa\n\$\\kappa_{0}\$ = $(round(first(sigma); digits=4)) MPa\n\$C\$ = $c \$\\frac{1}{s}\$\n\$h_{0}\$ = $(round(h0; digits=4)) MPa\nE = $(round(error; digits=4)) \$\\frac{MPa}{MPa}\$"))
+            scatter!(ax_alpha, time .* 1e3, alpha)
+        end
+        savefig("./ENGR851_JobyAnthonyIII_Homework3/stress_strain-$temp.png")
         # finally:
         #     sys.stdout.close()
         #     sys.stdout = old_stdout
         #     print(DRAG)
+    end
+end
